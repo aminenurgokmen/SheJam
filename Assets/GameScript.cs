dd38faff522a18d4d38e010680979a5991ead09a
Aminenur Gökmen <93777622+aminenurgokmen@users.noreply.github.com>
2025-10-25 00:24:44 +0300

    fiup

---
+++ Assets/GameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
